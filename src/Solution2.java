6238e62e70e45dcde2ca65723c2734be705f85ef
sunghuncho <chohoon901@gmail.com>
2023-04-18 17:00:52 +0900

    Initial commit

---
+++ src/Solution2.java
@@ -0,0 +1,14 @@
+public class Solution2 {
+    public static void main(String[] args) {
+//        System.out.print(i);
+        int i = 0;
+        long i2 = 999999999999L;
+        float f = 1.5f;
+        float f2 = 1.5555555555f;
+        System.out.println(f);
+        System.out.println(f2);
+        double d = 1.5;
+        System.out.println(d);
+
+    }
+}
